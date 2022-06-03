f5e8749aab75585dc0278c85e5022cc1272f5a68
Christof Fetzer <christof.fetzer@scontain.com>
2022-06-03 14:10:50 +0200

    updated meshfile to include PV claims

--- run.sh
+++ run.sh
@@ -1,16 +1,6 @@
 #/bin/bash
 
-alias scone="docker run -it --rm \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v \"$HOME/.docker:/root/.docker\" \
-    -v \"$HOME/.cas:/root/.cas\" \
-    -v \"$HOME/.scone:/root/.scone\" \
-    -v \"\$PWD:/root\" \
-    -w /root \
-    registry.scontain.com:5050/cicd/sconecli scone"
+sconectl apply -f FastApi.yml   # generates the FastAPI image
+sconectl apply -f Meshfile.yml  # generates and uploads the policies and writes the 
 
-scone apply -f FastApi.yml   # generates the FastAPI image
-scone apply -f Meshfile.yml  # generates and uploads the policies and writes the 
-
-kubectl create namespace SecureDocuments # ensure that we have the right name
-helm install --namespace SecureDocuments secure-doc-management # use helm chart to install
+helm install secure-doc-management target/helm
